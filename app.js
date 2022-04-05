1cca25bbbb1a55987ed040c903a1e533d6be9b26
Lartg <roc.gerrety@gmail.com>
2022-04-04 18:31:34 -0700

    init project

---
+++ app.js
@@ -0,0 +1,37 @@
+// Require Libraries
+const fetch = import('node-fetch');
+const express = require('express');
+const methodOverride = require('method-override')
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const bodyParser = require('body-parser');
+
+
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// Middleware
+const { engine } = require('express-handlebars');
+
+app.engine('handlebars', engine({
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+}));
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const jwt = require('jsonwebtoken');
+
+require('./controllers/feed')(app);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('App listening on port 3000')
+})
